2e60c8d70f6b76a5432b89ad7496f0bcc6aaf799
tita30 <118965084+tita30@users.noreply.github.com>
2022-11-26 06:50:43 +0700

    Initial commit

---
+++ CRUD_with_kolit_fan/app/src/main/java/com/titawulandarin/crud_with_kolit_fan/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.titawulandarin.crud_with_kolit_fan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
